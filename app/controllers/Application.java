7229f216bbf464c1e69976954ec872d0e2a8897b
Gidro <Gidroshvandel@gmail.com>
2016-09-11 23:11:44 +0300

    add standards DB rows

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -21,7 +21,27 @@ public class Application extends Controller {
     static void setConnectedUser() {
         if(Security.isConnected()) {
             if(User.findByEmail("admin@mail.ru")==null){
-                new User("admin@mail.ru","root","admin",true).save();
+                try {
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                    new User("admin@mail.ru","root","admin",true).save();
+                    User user = new User("user@mail.ru","123456","Иванов Иван Иванович",false).save();
+                    User user2 = new User("user2@mail.ru","123456","Простакова Людмила Сергеевна",false).save();
+                    Book book = new Book("Война и мир","Лев Николаевич Толстой").save();
+                    Book book2 = new Book("Степной Волк","Герман Гёссе").save();
+                    Book book3 = new Book("Фауст","Иоганн Вольфганг фон Гёте").save();
+                    new Book("Процесс","Франц Кафка").save();
+                    new Book("Цифровой","Сергей и Мария Дяченко").save();
+                    new Library(book, user, formatter.parse("2016-09-9"),formatter.parse("2016-09-11"),false).save();
+                    new Library(book2, user, formatter.parse("2016-09-1"),formatter.parse("2016-09-9"),false).save();
+                    new Library(book3, user, formatter.parse("2016-09-11"),formatter.parse("2016-09-16"),true).save();
+                    new Library(book, user2, formatter.parse("2016-09-2"),formatter.parse("2016-09-11"),false).save();
+                    new Library(book2, user2, formatter.parse("2016-09-5"),formatter.parse("2016-09-15"),true).save();
+                    new Library(book3, user2, formatter.parse("2016-09-11"),formatter.parse("2016-09-22"),true).save();
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+
             }
             User user = User.find("byEmail", Security.connected()).first();
             renderArgs.put("user", user.getFullname());
@@ -51,6 +71,7 @@ public class Application extends Controller {
         try{
             Library lib = Library.findById(id);
             lib.setOnUse(false);
+//            lib.setEndReadDate(new java.util.Date ());
             lib.save();
             String e = "Возврат прошёл успешно";
             render(e);
